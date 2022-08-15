6fa8791af2caff2470ee7f9398381f33bd779503
Vinicius MOntanha <viniciusmassis01@hotmail.com>
2022-08-15 13:05:29 -0300

    final commit with splash

--- App.js
+++ App.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Onboarding from './src/views/Onboarding'
 import Calculator from './src/views/Calculator';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LottieView from 'lottie-react-native';
 
 const AppStack = createNativeStackNavigator();
 
 export default function App() {
-          return (
-                    <>
+          const [loaded, setLoaded] = useState(false);
+          if (loaded == false) {
+                    return (
+                              <LottieView
+                                        source={require('./assets/g-logo.json')}
+                                        autoPlay
+                                        loop={false}
+                                        resizeMode='contain'
+                                        onAnimationFinish={() => {
+                                                  setLoaded(true);
+                                        }}
+                              />
+
+                    );
+          } else {
+                    return (
+
                               <NavigationContainer>
-                                        <AppStack.Navigator initialRouteName='Onboarding'>
+                                        <AppStack.Navigator initialRouteName='SplashScreen'>
                                                   <AppStack.Screen name="Onboarding"
-                                                            options={{
-                                                                      title: 'Bem-vindo(a)',
-                                                                      headerStyle: {
-                                                                                backgroundColor: '#000',
-                                                                      },
-                                                                      headerTintColor: '#fff',
-                                                                      headerTitleStyle: {
-                                                                                fontWeight: 'bold',
-                                                                      },
-                                                            }}
+                                                            options={{ headerShown: false }}
                                                             component={Onboarding} />
+                                                  {/* <AppStack.Screen name="SplashScreen"
+                                                            options={{ headerShown: false }}
+                                                            component={SplashScreen} /> */}
 
                                                   <AppStack.Screen name="Calculator"
                                                             options={{
                                                                       headerBackVisible: false,
                                                                       title: '',
+                                                                      headerShown: false,
                                                                       headerStyle: {
                                                                                 backgroundColor: '#000',
                                                                       },
@@ -39,6 +50,6 @@ export default function App() {
                                                             component={Calculator} />
                                         </AppStack.Navigator>
                               </NavigationContainer>
-                    </>
-          );
+                    );
+          }
 }
--- src/views/Onboarding.js
+++ src/views/Onboarding.js
@@ -2,30 +2,31 @@
 import { Image } from 'react-native';
 import React from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
-
+import { StatusBar } from 'expo-status-bar';
 const Simple = ({ navigation }) => (
           <>
+                    <StatusBar style='auto' />
                     <Onboarding
                               onDone={() => navigation.navigate('Calculator')}
                               onSkip={() => navigation.navigate('Calculator')}
                               pages={[
                                         {
-                                                  backgroundColor: '#d9d9d9',
-                                                  image: <Image source={require('../../assets/splash.png')} style={{ width: 200, height: 200 }} />,
-                                                  title: '',
-                                                  subtitle: '',
+                                                  backgroundColor: '#01b6bd',
+                                                  image: <Image source={require('../../assets/1.png')} style={{ width: 200, height: 200 }} />,
+                                                  title: 'Calculadora IOS 7',
+                                                  subtitle: 'Seja bem vindo a mais um desenvolvimento.',
                                         },
                                         {
-                                                  backgroundColor: '#000',
-                                                  image: <Image source={require('../../assets/icon.png')} style={{ width: 90, height: 90 }} />,
-                                                  title: 'The Title',
-                                                  subtitle: 'This is the subtitle that sumplements the title.',
+                                                  backgroundColor: '#1a1f25',
+                                                  image: <Image source={require('../../assets/2.png')} style={{ width: 200, height: 200 }} />,
+                                                  title: 'Aprendizado',
+                                                  subtitle: 'Este estudo tem apenas como finalidade criar e distribuir das novas versões de publicações e desenvolvimento, boas práticas e novas tecnologias.',
                                         },
                                         {
-                                                  backgroundColor: '#999',
-                                                  image: <Image source={require('../../assets/splash.png')} style={{ width: 90, height: 90 }} />,
-                                                  title: 'Triangle',
-                                                  subtitle: "Beautiful, isn't it?",
+                                                  backgroundColor: '#fff',
+                                                  image: <Image source={require('../../assets/3.png')} style={{ width: 200, height: 200 }} />,
+                                                  title: 'Bem vindo (a)',
+                                                  subtitle: 'Corre sempre para novas atualizações',
                                         },
                               ]}
                     />
